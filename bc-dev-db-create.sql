6e18cbd88f5eff98a78babf10f30b50c4ffdc730
Sam O'Connell <samuel.oconnell1493@gmail.com>
2020-03-18 19:10:15 -0400

    created working table

--- bc-dev-db-create.sql
+++ bc-dev-db-create.sql
@@ -4,29 +4,16 @@ create database sqlbootcamp_developer0318;
 use sqlbootcamp_developer0318;
 
 -- create the customer table
-create table developer (
-	emp_id 				int 			not null primary key auto_increment,
+create table developer(
+	emp_id 				int 			not null primary key,
 	first_name 			varchar(50) 	not null,
     last_name 			varchar(50) 	not null,
 	role 				varchar(50) 	not null,
 	salary 				int 			not null default 0
 );
 
--- add some data to customer table
-Insert into  (name, city, state, isCorpAcct, creditLimit)
-values ('Mercury LTD', 'Newport', 'KY', 0, 100000);
-Insert into customer (name, city, state, isCorpAcct, creditLimit)
-values ('Venus LLC', 'Dayton', 'OH', 0, 300000);
-Insert into customer (name, city, state, isCorpAcct, creditLimit)
-values ('Mars INC', 'Cincinnati', 'OH', 1, 1000000);
-Insert into customer (name, city, state, isCorpAcct, creditLimit)
-values ('Jupiter LPA', 'Covington', 'KY', 0, 250000);
-Insert into customer (name, city, state, isCorpAcct, creditLimit)
-values ('Saturn INC', 'Batesville', 'IN', 0, 300000);
-Insert into customer (name, city, state, isCorpAcct, creditLimit)
-values ('Uranus LPA', 'Indianapolis', 'IN', 1, 1000000);
-Insert into customer (name, city, state, isCorpAcct, creditLimit)
-values ('Neptune INC', 'Cleveland', 'OH', 1, 1000000);
+Insert into developer (emp_id, first_name, last_name, role, salary)
+	values(4512, 'Jeff', 'Peters', 'Developer 1', 62000);
 
 
 -- this is a comment
