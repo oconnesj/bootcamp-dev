71fd3f8bd46b4aea00f2188c556ff08801c635f3
Sam O'Connell <samuel.oconnell1493@gmail.com>
2020-03-18 18:46:25 -0400

    fresh start before editing table

--- bc-dev-db-create.sql
+++ bc-dev-db-create.sql
@@ -1,10 +1,10 @@
 -- create the database schema
-drop database if exists sqlbootcamp_mod4;
-create database sqlbootcamp_mod4;
-use sqlbootcamp_mod4;
+drop database if exists sqlbootcamp_developer0318;
+create database sqlbootcamp_developer0318;
+use sqlbootcamp_developer0318;
 
 -- create the customer table
-create table customer (
+create table developer (
 	id 				int 			not null primary key auto_increment,
 	name 			varchar(50) 	not null,
 	city 			varchar(50) 	not null,
@@ -30,14 +30,7 @@ values ('Uranus LPA', 'Indianapolis', 'IN', 1, 1000000);
 Insert into customer (name, city, state, isCorpAcct, creditLimit)
 values ('Neptune INC', 'Cleveland', 'OH', 1, 1000000);
 
--- create the orders table
-create table orders (
-	id 				int 			not null primary key auto_increment,
-	customerID 		int 			not null,
-	date			datetime 		not null,
-	total 			decimal(14,2) 	not null default 0,
-    FOREIGN KEY (customerID) REFERENCES customer(id)
-);
+
 -- this is a comment
 -- code below does the same thing as foreign key declaration above
 -- ALTER TABLE orders
@@ -45,26 +38,3 @@ create table orders (
 /*    this is a multi
       line comment */
 
--- add some orders to our orders table
-Insert into orders (date, total, customerID)
-values ('2017-12-31', 778, 6);
-Insert into orders (date, total, customerID)
-values ('2017-7-3', 156, 5);
-Insert into orders (date, total, customerID)
-values ('2017-6-23', 491, 7);
-Insert into orders (date, total, customerID)
-values ('2017-3-6', 230, 6);
-Insert into orders (date, total, customerID)
-values ('2017-11-21', 844, 5);
-Insert into orders (date, total, customerID)
-values ('2017-9-18', 849, 2);
-Insert into orders (date, total, customerID)
-values ('2017-4-15', 371, 2);
-Insert into orders (date, total, customerID)
-values ('2017-7-13', 451, 2);
-Insert into orders (date, total, customerID)
-values ('2017-1-7', 510, 4);
-Insert into orders (date, total, customerID)
-values ('2017-9-23', 612, 6);
-Insert into orders (date, total, customerID)
-values ('2017-10-11', 736, 5);
